d18acaeef951ccb8421b18c87379ebb8032dfc60
Russell <rufletch@berkeley.edu>
2017-09-13 21:04:16 -0700

    print files on which normalization is run

--- scripts/oeHBC_exclude.R
+++ scripts/oeHBC_exclude.R
@@ -6,6 +6,7 @@ library(SummarizedExperiment)
 out_dir <- "../output/clust/oeHBC"
 
 normfiles=list.files(path = out_dir, pattern=glob2rx("oeHBC_*fq*_se.Rda"))
+print(normfiles)
 
 reg3g_list_all = list()
 omp_cyp_list_all = list()
