14ce2e4d91f8510a9e85744b8e369c5e126192ee
Diya Das <diyadas@users.noreply.github.com>
2017-09-18 19:59:29 -0700

    minor naming

--- scripts/oeHBCregen_OR.R
+++ scripts/oeHBCregen_OR.R
@@ -1,12 +1,14 @@
 #title: Olfactory Receptors and Chromatin Regulators/TFs in the Neuronal Lineage
 #author: Diya Das
 
+library(clusterExperiment)
+
 expt_str <- "oeHBCregenWT"
 out_dir <- file.path("../output/clust", expt_str)
 viz_dir <- file.path("../output/viz", expt_str)
 
 load(file.path(out_dir, paste0(expt_str, "_slingshot_out.Rda")))
-load(file.path(out_dir, paste0(expt_str, "_LineageData2.Rda")))
+load(file.path(out_dir, paste0(expt_str, "_LineageData.Rda")))
 load(file.path(out_dir, paste0(expt_str,"_PCA.Rda")))
 load("../data/oeHBC_RSEM_eSet.Rda")
 
@@ -19,7 +21,7 @@ linedf <- linedf[complete.cases(linedf),]
 
 ors <- rownames(RSEM_eSet)[grep("^Olfr[^ps]*$",rownames(RSEM_eSet))]
 or_names <- sapply(ors, function(x) strsplit(x,"\\.")[[1]][1])
-or_counts <- data.frame(assayData(RSEM_eSet)$tpm_table[ors,colnames(nlm2)])
+or_counts <- data.frame(assayData(RSEM_eSet)$tpm_table[ors,colnames(nlm)])
 or_counts[is.na(or_counts)] <- 0
 or_sums <- sapply(or_counts, function(x) tapply(x, or_names,sum))
 
@@ -48,8 +50,8 @@ title(xlab="Developmental Order")
 ngenes = c("Ehmt1", "Setdb1","Setdb2","Kdm1a", "Lbr","Lhx2","Ebf1","Ebf3", "Ebf4", "Gnai2","Gnas","Gnal","Gnb1", "Gng8","Gng13","Gng3","Adcy3","Ano2","Cnga2","Cnga4","Atf5","Rtp1")
 for (i in 1:length(ngenes)){
 gene = ngenes[i]
-gene_exp = 2^(nlm2[gene,])-1
-plot(1:ncol(nlm2),gene_exp, pch=16,xlab='',ylim=c(1,max(gene_exp)),ylab='Counts', col=scales::alpha(col.pal[linedf$clus.labels],0.5),xaxt='n',yaxt='n', cex=0.5)
+gene_exp = 2^(nlm[gene,])-1
+plot(1:ncol(nlm),gene_exp, pch=16,xlab='',ylim=c(1,max(gene_exp)),ylab='Counts', col=scales::alpha(col.pal[linedf$clus.labels],0.5),xaxt='n',yaxt='n', cex=0.5)
 text(par()$usr[1],par()$usr[4],gene, adj = c(-0.1,1.2), cex=0.6) #0.6
 axis(2, lwd = 0.5, tck=-0.01*2)
 if (gene %in% c("Ebf4","Gng3","Rtp1")){
