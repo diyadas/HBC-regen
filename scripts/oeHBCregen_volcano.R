f96e52fb2a0bea663c921833575953b9f779739b
Diya Das <diyadas@users.noreply.github.com>
2017-10-20 20:17:54 -0700

    tidied script; fixed margins

--- scripts/oeHBCregen_volcano.R
+++ scripts/oeHBCregen_volcano.R
@@ -1,6 +1,7 @@
-#title: Volcano Plots
-#author: Diya Das
-#date: April 6, 2017
+# title: Volcano Plots for Lineage Transitions in Regeneration
+# author: Diya Das
+# date: April 6, 2017
+
 
 library(clusterExperiment); library(calibrate); library(plyr)
 expt_str <- "oeHBCregenWT"
@@ -26,6 +27,27 @@ contrast_name <- data.frame(contrast = paste0("X",contrast_tuple$X1,"-X",contras
 
 genes_to_label = c("Trp63","Krt5","Krt14","Cyp2g1","Notch2","Cyp1a2","Trpm5","Ascl3","Cftr","Sox9", "Kit","Ascl1","Ccnd1","Top2a","Neurod1","Lhx2","Gap43","Gng8","Omp","Cnga2","Gng13", "Reg3g", "Krtdap","Sprr1a", "Krt16")
 
+label_markers <- function(cont_sub, pval_thresh, genes_to_label){
+  # function to label top 6 genes + selected markers
+  with(subset(cont_sub, adj.P.Val<pval_thresh & abs(logFC)>1), {
+    n = unique(c(na.omit(order(logFC)[1:6]),
+                 na.omit(order(logFC, decreasing = T)[1:6]),
+                 na.omit(match(genes_to_label,Feature))))
+    n1 <- vector(); n2 <- vector()
+    invisible(lapply(n, function(z) {
+      if (logFC[z] > 0) {
+        n1 <<- c(n1,z)
+      }else{
+        n2 <<- c(n2,z)
+      }
+    }))
+    points(logFC[n1], -log10(adj.P.Val[n1]), pch=1, col=c("firebrick4"), cex = 0.7)
+    textxy(logFC[n1], -log10(adj.P.Val[n1]), labs=Feature[n1], cex=.5, offset=0.7, col=c("firebrick4"))
+    points(logFC[n2], -log10(adj.P.Val[n2]), pch=1, col=c("dodgerblue4"), cex = 0.7)
+    textxy(logFC[n2], -log10(adj.P.Val[n2]), labs=Feature[n2], cex=.5, offset=0.7, col=c("dodgerblue4"))
+  })
+}
+
 tmplist <- apply(contrast_tuple, 1, function(x){
   desired_contrast <- paste0("X",x[1],"-X",x[2])
   opposite_contrast <- paste0("X",x[2],"-X",x[1])
@@ -36,7 +58,6 @@ xlim_1 <- round_any(max(abs(pairsDE$logFC)), 1, f=ceiling)
 ylim_2 <- round_any(max(-log10(pairsDE_sub$adj.P.Val)), 10, f = ceiling)
 
 plotContrasts <- function(DE, contrast_tuple, contrast_name, fname, pval_thresh){
-  par(family='URWHelvetica', cex.lab=0.5, cex.axis=0.5, oma=c(0,0,0,0), mgp=c(1.2,0.15,0),xpd=F, lwd=0.5, pty="s", mar=c(0,2,1,1))
   write(c("Contrast name", "Positive Change", "Negative change", paste("max_pval <", pval_thresh)), file=fname, sep = "\t", append=FALSE, ncolumns = 4)
   tmplist <- apply(contrast_tuple, 1, function(x){
     desired_contrast <- paste0("X",x[1],"-X",x[2])
@@ -50,89 +71,44 @@ plotContrasts <- function(DE, contrast_tuple, contrast_name, fname, pval_thresh)
       stop('stop something wrong')
     }
     
-    # plotting
-    png(file.path(DE_dir,paste0(expt_str, "_", contrast_name$names[contrast_name$contrast==desired_contrast],"_volcano_",Sys.Date(),".png")), width=2, height = 2, units="in", res=600)
-    par(family='Helvetica', cex.lab=0.5, cex.axis=0.5, oma=c(0,0,0,0), mgp=c(1.2,0.15,0),xpd=F, lwd=0.5, pty="s", mar=c(0.3,0.3,0.3,0.3))
-    #layout(c(2,1,1,1))
-    with(cont_sub, plot(logFC, -log10(adj.P.Val),xlim=c(-xlim_1,xlim_1), pch=20, col="grey", ylim=c(0,90),cex=0.5, xaxt='n',yaxt='n',xlab='',ylab=''))
-    axis(1, lwd = 0.5, tck=-0.01*2, labels=F);axis(2, lwd = 0.5, tck=-0.01*2, labels=F)
+    # plotting all genes
+    png(file.path(DE_dir,paste0(expt_str, "_", contrast_name$names[contrast_name$contrast==desired_contrast],"_volcano_",Sys.Date(),".png")), width = 2.25, height = 2.25, units="in", res=600)
+    par(family='Helvetica', cex.lab=0.5, cex.axis=0.5, oma=c(0,0,0,0), mgp=c(0.8,0.15,0), xpd=F, lwd=0.5, pty="s", mar=c(1.7,1.7,0.3,0.3))
+    with(cont_sub, plot(logFC, -log10(adj.P.Val),xlim=c(-xlim_1,xlim_1), pch=20, col="grey", ylim=c(0,90),cex=0.5, xaxt='n',yaxt='n',xlab='logFC',ylab='-log10(adj. p-value)'))
+    axis(1, lwd = 0.5, tck=-0.01*2, labels=T);axis(2, lwd = 0.5, tck=-0.01*2, labels=T)
     with(subset(cont_sub, adj.P.Val<pval_thresh & abs(logFC)>1), points(logFC, -log10(adj.P.Val), pch=20, col="black", cex=0.5))
     
-    #par(mar=c(0.2,1,1,1))
-    #with(cont_sub, plot(logFC, -log10(adj.P.Val),xlim=c(-xlim_1,xlim_1), pch=20, col="grey", ylim=c(100,ylim_2),cex=0.5, xaxt='n',yaxt='n',xlab='',ylab=''))
-    #axis(2, lwd = 0.5, tck=-0.01*2, labels=TRUE)
-    #with(subset(cont_sub, adj.P.Val<pval_thresh & abs(logFC)>1), points(logFC, -log10(adj.P.Val), pch=20, col="black", cex=0.5))
+    # label number of DE genes
+    neg_num <- with(subset(cont_sub, adj.P.Val<pval_thresh & logFC < -1), length(Feature))
+    pos_num <- with(subset(cont_sub, adj.P.Val<pval_thresh & logFC > 1), length(Feature))
+    usr <- par( "usr" )
+    text( usr[ 1 ], usr[ 4 ], neg_num, adj = c( -0.5, 2 ), col = "dodgerblue4", font=4)
+    text( usr[ 2 ], usr[ 4 ], pos_num, adj = c( 1.5, 2 ), col = "firebrick4", font=4)
     
-   #  # label number of DE genes
-   #  neg_num <- with(subset(cont_sub, adj.P.Val<pval_thresh & logFC < -1), length(Feature))
-   #  pos_num <- with(subset(cont_sub, adj.P.Val<pval_thresh & logFC > 1), length(Feature))
-   #  usr <- par( "usr" )
-   #  text( usr[ 1 ], usr[ 4 ], neg_num, adj = c( -0.5, 2 ), col = "dodgerblue4", font=4)
-   #  text( usr[ 2 ], usr[ 4 ], pos_num, adj = c( 1.5, 2 ), col = "firebrick4", font=4)
-   #  
-   # with(subset(cont_sub, adj.P.Val<pval_thresh & abs(logFC)>1), {
-   #    n = unique(c(na.omit(order(logFC)[1:6]),
-   #                 na.omit(order(logFC, decreasing = T)[1:6]),
-   #                 na.omit(match(genes_to_label,Feature))))
-   #    n1 <- vector(); n2 <- vector()
-   #    invisible(lapply(n, function(z) {
-   #      if (logFC[z] > 0) {
-   #        n1 <<- c(n1,z)
-   #      }else{
-   #        n2 <<- c(n2,z)
-   #      }
-   #    }))
-   #    points(logFC[n1], -log10(adj.P.Val[n1]), pch=1, col=c("firebrick4"), cex = 0.7)
-   #    textxy(logFC[n1], -log10(adj.P.Val[n1]), labs=Feature[n1], cex=.5, offset=0.7, col=c("firebrick4"))
-   #    points(logFC[n2], -log10(adj.P.Val[n2]), pch=1, col=c("dodgerblue4"), cex = 0.7)
-   #    textxy(logFC[n2], -log10(adj.P.Val[n2]), labs=Feature[n2], cex=.5, offset=0.7, col=c("dodgerblue4"))
-   #  })
-    
+    # label_markers(cont_sub, pval_thresh, genes_to_label) # sanity check
     dev.off()
     
+    # plotting only transcription factors
+    png(file.path(DE_dir,paste0(expt_str, "_", contrast_name$names[contrast_name$contrast==desired_contrast],"_tf_volcano_",Sys.Date(),".png")), width = 2.25, height = 2.25, units="in", res=600)
+    par(family='Helvetica', cex.lab=0.5, cex.axis=0.5, oma=c(0,0,0,0), mgp=c(0.8,0.15,0),xpd=F, lwd=0.5, pty="s", mar=c(1.7,1.7,0.3,0.3))
     
-    png(file.path(DE_dir,paste0(expt_str, "_", contrast_name$names[contrast_name$contrast==desired_contrast],"_tf_volcano_",Sys.Date(),".png")), width=2, height = 2, units="in", res=600)
-    par(family='Helvetica', cex.lab=0.5, cex.axis=0.5, oma=c(0,0,0,0), mgp=c(1.2,0.15,0),xpd=F, lwd=0.5, pty="s", mar=c(0.3,0.3,0.3,0.3))
-    #layout(c(2,1,1,1))
-    with(subset(cont_sub, Feature %in% tfs), plot(logFC, -log10(adj.P.Val),xlim=c(-xlim_1,xlim_1), pch=20, col="grey", ylim=c(0,90),cex=0.5, xaxt='n',yaxt='n',xlab='',ylab=''))
-    axis(1, lwd = 0.5, tck=-0.01*2, labels=F);axis(2, lwd = 0.5, tck=-0.01*2, labels=F)
+    with(subset(cont_sub, Feature %in% tfs), plot(logFC, -log10(adj.P.Val),xlim=c(-xlim_1,xlim_1), pch=20, col="grey", ylim=c(0,90),cex=0.5, xaxt='n',yaxt='n',xlab='logFC',ylab='-log10(adj. p-value)'))
+    axis(1, lwd = 0.5, tck=-0.01*2, labels=T);axis(2, lwd = 0.5, tck=-0.01*2, labels=T)
     with(subset(cont_sub, adj.P.Val<pval_thresh & abs(logFC)>1 & Feature %in% tfs), points(logFC, -log10(adj.P.Val), pch=20, col="black", cex=0.5))
     
-    
-    # #label number of DE genes
-    # neg_num <- with(subset(cont_sub, adj.P.Val<pval_thresh & logFC < -1 & Feature %in% tfs), length(Feature))
-    # pos_num <- with(subset(cont_sub, adj.P.Val<pval_thresh & logFC > 1 & Feature %in% tfs), length(Feature))
-    # usr <- par( "usr" )
-    # text( usr[ 1 ], usr[ 4 ], neg_num, adj = c( -0.5, 2 ), col = "dodgerblue4", font=4)
-    # text( usr[ 2 ], usr[ 4 ], pos_num, adj = c( 1.5, 2 ), col = "firebrick4", font=4)
-    # 
-    # with(subset(cont_sub, adj.P.Val<pval_thresh & abs(logFC)>1), {
-    #    n = unique(c(na.omit(order(logFC)[1:6]),
-    #                 na.omit(order(logFC, decreasing = T)[1:6]),
-    #                 na.omit(match(genes_to_label,Feature))))
-    #    n1 <- vector(); n2 <- vector()
-    #    invisible(lapply(n, function(z) {
-    #      if (logFC[z] > 0) {
-    #        n1 <<- c(n1,z)
-    #      }else{
-    #        n2 <<- c(n2,z)
-    #      }
-    #    }))
-    #    points(logFC[n1], -log10(adj.P.Val[n1]), pch=1, col=c("firebrick4"), cex = 0.7)
-    #    textxy(logFC[n1], -log10(adj.P.Val[n1]), labs=Feature[n1], cex=.5, offset=0.7, col=c("firebrick4"))
-    #    points(logFC[n2], -log10(adj.P.Val[n2]), pch=1, col=c("dodgerblue4"), cex = 0.7)
-    #    textxy(logFC[n2], -log10(adj.P.Val[n2]), labs=Feature[n2], cex=.5, offset=0.7, col=c("dodgerblue4"))
-    #  })
-
-    
+    #label number of DE genes
+    neg_num <- with(subset(cont_sub, adj.P.Val<pval_thresh & logFC < -1 & Feature %in% tfs), length(Feature))
+    pos_num <- with(subset(cont_sub, adj.P.Val<pval_thresh & logFC > 1 & Feature %in% tfs), length(Feature))
+    usr <- par( "usr" )
+    text( usr[ 1 ], usr[ 4 ], neg_num, adj = c( -0.5, 2 ), col = "dodgerblue4", font=4)
+    text( usr[ 2 ], usr[ 4 ], pos_num, adj = c( 1.5, 2 ), col = "firebrick4", font=4)
     
     dev.off()
-
-    with(subset(cont_sub, adj.P.Val<pval_thresh & abs(logFC)>1), write(c(contrast_name$names[contrast_name$contrast==desired_contrast], sum(logFC>0), sum(logFC<0), max(adj.P.Val)), ncolumns=4, file=fname, sep = "\t", append=TRUE))
     
-      })
-} 
+    with(subset(cont_sub, adj.P.Val<pval_thresh & abs(logFC)>1), write(c(contrast_name$names[contrast_name$contrast==desired_contrast], sum(logFC>0), sum(logFC<0), max(adj.P.Val)), ncolumns=4, file=fname, sep = "\t", append=TRUE))
+  })
+}  
 
-pval_thresh=1e-2
+pval_thresh <- 1e-2
 
-plotContrasts(pairsDE, contrast_tuple, contrast_name, fname=file.path(DE_dir,paste0(expt_str,"_pairsDE_tuple_p",pval_thresh,"_FC1_2_",Sys.Date(),".txt")), pval_thresh)
+plotContrasts(pairsDE, contrast_tuple, contrast_name, fname = file.path(DE_dir,paste0(expt_str,"_pairsDE_tuple_p",pval_thresh,"_FC1_2_",Sys.Date(),".txt")), pval_thresh)
