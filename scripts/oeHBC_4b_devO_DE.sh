cdba7292a16284ed1454dc143644cf49d9fe15ab
Diya Das <diyadas@users.noreply.github.com>
2017-10-20 20:23:18 -0700

    minor formatting

--- scripts/oeHBC_4b_devO_DE.sh
+++ scripts/oeHBC_4b_devO_DE.sh
@@ -4,7 +4,7 @@ NOW=$(date +"%m%d%Y_%H%M%S")
 
 module load R-3.3.1
 
-R --vanilla<<<"rmarkdown::render('oeHBCregenWT_slingshot.Rmd')"
-R --vanilla<<<"rmarkdown::render('oeHBCregenWTKO_slingshot.Rmd')"
-R --vanilla<<<"rmarkdown::render('oeHBCregenWT_DE.Rmd')"
-R --vanilla<<<"rmarkdown::render('oeHBCregenWTKO_DE.Rmd')"
+R --vanilla <<< "rmarkdown::render('oeHBCregenWT_slingshot.Rmd')"
+R --vanilla <<< "rmarkdown::render('oeHBCregenWTKO_slingshot.Rmd')"
+R --vanilla <<< "rmarkdown::render('oeHBCregenWT_DE.Rmd')"
+R --vanilla <<< "rmarkdown::render('oeHBCregenWTKO_DE.Rmd')"
--- scripts/oeHBC_5_GSEA.sh
+++ scripts/oeHBC_5_GSEA.sh
@@ -6,6 +6,6 @@ nrot=10000000
 NOW=$(date +"%m%d%Y_%H%M%S")
 module load R-3.3.1
 
-R --vanilla <<<"rmarkdown::render('oeHBCregenWT_GSEAprep.Rmd')"
+R --vanilla <<< "rmarkdown::render('oeHBCregenWT_GSEAprep.Rmd')"
 
 R --vanilla < oeHBCregenWT_romerGSEA.R --args --expt oeHBCregenWT --ncores $ncores --nrot $nrot > "oeHBCregenWT_GSEA_"$NOW.Rout
